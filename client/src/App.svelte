e110a5fd99ee1a060093cee2d80168f7bf2fdd1b
Liam <liam@scalzulli.com>
2023-03-07 15:28:04 -0500

    Add judge0 back for running rust code

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -5,10 +5,19 @@
 
   let container;
 
+  let code = 'fn main() {\n  println!("Hello, world!"); \n}';
+
+  let error = '';
+
   onMount(() => {
-    monaco.editor.create(container, {
+    let editor = monaco.editor.create(container, {
+      value: code,
       language: 'rust',
       theme: 'vs-dark',
+    });
+
+    editor.onDidChangeModelContent(() => {
+      code = editor.getValue();
     });
   });
 
@@ -17,10 +26,63 @@
   let data = null;
 
   async function handleInput(event) {
-    const response = await fetch(
-      'http://localhost:8000/search?query=' + event.target.value
-    );
-    data = await response.json();
+    try {
+      const response = await fetch(
+        'http://localhost:8000/search?query=' + event.target.value
+      );
+      data = await response.json();
+    } catch (err) {
+      error = err.toString();
+    }
+  }
+
+  async function runCode() {
+    console.log(`Running code: ${code}...`);
+
+    try {
+      const response = await fetch('/judge/submissions', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          source_code: code,
+          language_id: 73,
+          stdin: '',
+          expected_output: '',
+          cpu_time_limit: 2,
+          wall_time_limit: 5,
+          memory_limit: 128000,
+        }),
+      });
+
+      const submission = await response.json();
+      const submissionId = submission.token;
+
+      console.log(submissionId);
+
+      let status = { description: 'Queue' };
+
+      while (
+        status.description !== 'Accepted' &&
+        status.description !== 'Compilation Error'
+      ) {
+        const result = await fetch(`/judge/submissions/${submissionId}`);
+        const data = await result.json();
+        console.log(data);
+        status = data.status;
+        console.log(status);
+        await new Promise((resolve) => setTimeout(resolve, 1000)); // Wait for 1 second before checking again
+      }
+
+      if (status.description === 'Accepted') {
+        const output = status.stdout;
+        console.log(output);
+      } else {
+        const error = status.compile_output || status.stderr;
+        console.error(error);
+      }
+    } catch (error) {
+      console.error(error);
+    }
   }
 </script>
 
@@ -28,7 +90,7 @@
   <h1 class="font-bold">rustground 🛝</h1>
 
   <label>
-    search: <input
+    search crates: <input
       class="border border-sky-500"
       type="text"
       bind:value={query}
@@ -47,5 +109,16 @@
     <!-- <p>{JSON.stringify(data)}</p> -->
   {/if}
 
-  <div bind:this={container} class="h-3/6" />
+  {#if error}
+    <p>{error}</p>
+  {/if}
+
+  <div bind:this={container} class="h-3/6 mt-2" />
+
+  <button
+    class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-2"
+    on:click={runCode}
+  >
+    Run
+  </button>
 </main>
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,16 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 export default defineConfig({
   root: './client',
   plugins: [svelte()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+      '/judge': {
+        target: 'http://localhost:2358',
+        rewrite: (path) => path.replace(/^\/judge/, ''),
+      },
+    },
+  },
 });
